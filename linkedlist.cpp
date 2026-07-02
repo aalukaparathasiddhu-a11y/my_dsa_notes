2ec5da416b75b3ffbe8acae11ef1e389783a6d94
aalukaparathasiddhu-a11y <aalukaparathasiddhu@gmail.com>
2026-07-02 21:59:20 +0530

    linked list
    deleting and inserting

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -40,3 +40,56 @@ head=newnode;
 return head;//returns head of new linked list
 }
 
+//inserting new element at end
+Node* insertatend(Node* head,int val){
+    Node*newnode=new Node(val);
+    Node* temp=head;
+    while(temp->next!=nullptr)temp=temp->next;
+    temp->next=newnode;
+    return head;
+}
+
+//inserting value at index
+Node* insertati(Node* head,int val,int ind){
+    Node*temp=head;
+    Node* newnode=new Node(val);
+    for(int i=0;i<ind && temp!=nullptr;i++){
+temp=temp->next;
+    }
+    if (temp==nullptr) {
+        delete newnode;
+    return head;
+}
+newnode->next=temp->next;
+temp->next=newnode;
+return head;
+}
+
+//deleting first element
+Node* deletehead(Node* head){
+    if(head==nullptr)return nullptr;
+    Node* temp=head;
+    head=head->next;
+    delete temp;
+    return head;
+}
+
+//deleting from middle
+Node* deleteNode(Node* head, int target) {
+    if (head == nullptr) return nullptr;
+    if (head->data == target) return deletehead(head);
+
+    Node* temp = head;
+    // Find the node BEFORE the target node
+    while (temp->next != nullptr && temp->next->data != target) {
+        temp = temp->next;
+    }
+
+    // If target was found
+    if (temp->next != nullptr) {
+        Node* nodeToDelete = temp->next;
+        temp->next = temp->next->next; // Link past the deleted node
+        delete nodeToDelete;
+    }
+    return head;
+}
